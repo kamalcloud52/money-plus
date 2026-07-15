0fb83ba1cceca49f1a0b8efcbd6c453b88f3672a
Kamal Fikar <amakita150@gmail.com>
2026-07-15 12:54:43 +0700

    Create Wallet.js

---
+++ pages/Wallet.js
@@ -0,0 +1,11 @@
+export function renderWallet() {
+    return `
+        <div class="app-container">
+            <div class="card placeholder-page">
+                <i class="fas fa-wallet"></i>
+                <h2>Wallet Page</h2>
+                <p>Halaman ini masih dalam pengembangan.</p>
+            </div>
+        </div>
+    `;
+}
